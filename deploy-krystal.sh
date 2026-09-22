4680a136540419d32751eb248322188fe457e9ae
jamesjlynch <info@jamesjlynch.com>
2026-09-22 16:03:56 +0100

    Use verified Krystal PHP 8.2 runtime

--- deploy-krystal.sh
+++ deploy-krystal.sh
@@ -6,12 +6,12 @@ cd /home/lynchde1/repositories/ankh-admin
 test -z "$(git status --porcelain)" || { echo 'Working tree has local changes; deployment stopped.'; exit 1; }
 git fetch origin main
 git merge --ff-only origin/main
-/usr/local/bin/php -l index.php
-/usr/local/bin/php -l setup.php
-/usr/local/bin/php -r 'if (PHP_VERSION_ID < 80100 || !extension_loaded("pdo_sqlite")) { fwrite(STDERR, "PHP 8.1+ and PDO SQLite required\n"); exit(1); }'
+/opt/cpanel/ea-php82/root/usr/bin/php -l index.php
+/opt/cpanel/ea-php82/root/usr/bin/php -l setup.php
+/opt/cpanel/ea-php82/root/usr/bin/php -r 'if (PHP_VERSION_ID < 80100 || !extension_loaded("pdo_sqlite")) { fwrite(STDERR, "PHP 8.1+ and PDO SQLite required\n"); exit(1); }'
 target=/home/lynchde1/public_html/ankh-admin
 mkdir -p "$target"
-for file in style.css setup.php index.php; do
+for file in .htaccess style.css setup.php index.php; do
   install -m 644 "$file" "$target/.$file.deploy"
   mv "$target/.$file.deploy" "$target/$file"
 done
